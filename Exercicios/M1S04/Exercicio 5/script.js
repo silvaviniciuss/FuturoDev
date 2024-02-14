a296069c29fed4bec2f5b9b8b9906ea44b2b82a4
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-14 20:46:35 -0300

    Exercicio 5

--- Exercicios/M1S04/Exercicio 5/script.js
+++ Exercicios/M1S04/Exercicio 5/script.js
@@ -4,12 +4,15 @@ const identificar = () => {
     if (numero > 0) {
         resultado.innerHTML = "POSITIVO"
         resultado.setAttribute("class", "blue")
+        console.log("POSITIVO")
     } else if ( numero < 0) {
         resultado.innerHTML = "NEGATIVO"
         resultado.setAttribute("class", "red")
+        console.log("NEGATIVO")
     } else {
         resultado.innerHTML = "IGUAL A ZERO"
         resultado.setAttribute("class", "zero")
+        console.log("IGUAL A ZERO")
     }
 
 }
