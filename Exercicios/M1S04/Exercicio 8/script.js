88b6199d871a210717ba8b6e49501e56c82b7d3c
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-15 17:33:23 -0300

    Exercicio 8

--- Exercicios/M1S04/Exercicio 8/script.js
+++ Exercicios/M1S04/Exercicio 8/script.js
@@ -1,7 +1,12 @@
-const funcaoParOuImpar = (numero) => {
+const funcaoParOuImpar = () => {
+    let numero = document.querySelector("#numero").value
+    let resultado = document.querySelector("#resultado")
+
     if (numero % 2 === 0) {
+        resultado.innerHTML = `O número ${numero} é PAR`
         console.log(`O número ${numero} é PAR`)
     } else {
+        resultado.innerHTML = `O número ${numero} é ÍMPAR`
         console.log(`O número ${numero} é ÍMPAR`)
     }
 }
