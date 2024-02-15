5172df493087efd3eada2a931fcca8f06ab12bbf
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-15 11:32:21 -0300

    Exercicio 10

--- Exercicios/M1S04/Exercicio 1/script.js
+++ Exercicios/M1S04/Exercicio 1/script.js
@@ -1,5 +1,7 @@
 const funcaoMensagem = () => {
     const mensagem = document.getElementById("exercicio1")
-    console.log(mensagem.value)
+    console.log((mensagem.value).toUpperCase())
+    alert(`${mensagem.value.toUpperCase()}`)
+    mensagem.value = " "
 }
 
---
+++ Exercicios/M1S04/Exercicio 10/script.js
@@ -0,0 +1,7 @@
+const idade = window.prompt("Qual é sua Idade? ")
+
+if(idade <= 18) {
+    console.log("Menor de Idade")
+} else {
+    console.log("Maior de Idade")
+}
