012964cfdd27dabbf48c2e783225ed61826e3527
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-14 22:00:55 -0300

    Exercicio 8

---
+++ Exercicios/M1S04/Exercicio 8/script.js
@@ -0,0 +1,10 @@
+const funcaoParOuImpar = (numero) => {
+    if (numero % 2 === 0) {
+        console.log(`O número ${numero} é PAR`)
+    } else {
+        console.log(`O número ${numero} é ÍMPAR`)
+    }
+}
+
+funcaoParOuImpar(3)
+funcaoParOuImpar(2)
