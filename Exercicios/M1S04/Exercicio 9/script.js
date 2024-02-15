338228ae26fea4385c1b147cdd8d110b2d6109ea
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-15 17:37:56 -0300

    Exercicio 9

--- Exercicios/M1S04/Exercicio 9/script.js
+++ Exercicios/M1S04/Exercicio 9/script.js
@@ -5,11 +5,11 @@ const mudarTexto = () => {
     const valor = select.options[select.selectedIndex].value
 
     switch (valor) {
-        case "texto1":
-            resultado.innerText = "Resultado do Texto 1"
+        case "html":
+            resultado.innerText = "HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores"
             break
-        case "texto2":
-            resultado.innerText = "Resultado do Texto 2"
+        case "css":
+            resultado.innerText = "Cascading Style Sheets é um mecanismo para adicionar estilos a uma página web, aplicado diretamente nas tags HTML ou ficar contido dentro das tags <style>"
             break
     }
 
