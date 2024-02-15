00e87ac94f89daeb0d7c905ffa540a189ab3e901
bandreid01 <viniciusdasilvax@gmail.com>
2024-02-15 13:12:04 -0300

    Exercicio 3

--- Exercicios/M1S04/Exercicio 3/script.js
+++ Exercicios/M1S04/Exercicio 3/script.js
@@ -2,7 +2,7 @@
 const funcaoNomeCompleto = () => {
     let nome = document.getElementById("text1")
     let sobrenome = document.getElementById("text2")
-    document.getElementById("nomeCompleto").innerHTML = `${nome.value} ${sobrenome.value}`
+    document.getElementById("nomeCompleto").innerText = `Nome completo: ${nome.value} ${sobrenome.value}`
 
     console.log(`${nome.value} ${sobrenome.value}`)
 }
